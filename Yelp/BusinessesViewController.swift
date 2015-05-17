4b71255175571155f7168c916fa540c10f8ed862
wansteve <swan@dropbox.com>
2015-05-17 14:07:59 -0700

    added a functional search bar

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,19 +8,36 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FiltersViewControllerDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FiltersViewControllerDelegate, UISearchBarDelegate {
     
     // implement these two protocols to set data source and capture table view events
     
     // declare myself, BusinessViewController as a delegate of the filtersviewcontroller in the last paratemter
 
     var businesses: [Business]! // where is the class Business defined?
+
+    lazy   var searchBars:UISearchBar = UISearchBar(frame: CGRectMake(0, 0, 200, 20))
+
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        var rightNavBarButton = UIBarButtonItem(customView: searchBars)
+        // self.navigationItem.leftBarButtonItem = leftNavBarButton
+       //  self.navigationItem.titleView = leftNavBarButton
+        //self.navigationItem.rightBarButtonItem = rightNavBarButton
+
+        self.searchBars = UISearchBar()
+        searchBars.delegate = self
+
+        searchBars.placeholder = "e.g. Thai, Restaurants"
+        self.navigationItem.titleView = searchBars
+        
+        
+        
         // initialize tableview with the following 2 lines
         tableView.delegate = self
         tableView.dataSource = self
@@ -29,7 +46,7 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         
         
         
-        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+        Business.searchWithTerm(searchBars.text, completion: { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
             self.tableView.reloadData() // if table is blank, need to add this reload inside viewDidLoad()
            
@@ -48,7 +65,33 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
             }
         }
     } // end of viewDidLoad
+    
 
+    func searchBarSearchButtonClicked(searchBars: UISearchBar) {
+        // self.clearResults()
+        // self.performSearch(searchBars.text)
+        
+        println("search bar changed")
+        Business.searchWithTerm(searchBars.text, completion: { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData() // if table is blank, need to add this reload inside viewDidLoad()
+            
+            for business in businesses {
+                println(business.name!)
+                println(business.address!)
+            }
+        })
+    }// searchBarSearchButton
+
+    
+/*
+    func clearResults() {
+        self.results = []
+        self.offset = 0
+        self.onResultsCleared()
+    }
+*/
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -98,6 +141,11 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     func filtersViewController(filtersViewController: FiltersViewController, didUpdateFilters filters: [String : AnyObject]) {
         
         var categories = filters["categories"] as? [String]
+        
+        //Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+        // var deals = filters["deals"] as? bool
+        // var sort = filters["sort") as? [String]
+        
         
         Business.searchWithTerm("Restaurants", sort: nil, categories: categories, deals: nil) {(businesses: [Business]!, error: NSError!) -> Void in
         self.businesses = businesses
